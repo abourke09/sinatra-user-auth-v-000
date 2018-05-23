d73b08e1fb6c892f98c2b7534118a6db377b1f39
Annie Bourke <aabourke@gmail.com>
2018-05-23 18:05:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
+    @user = User.create(email: params["email"], password: params["password"])
 
     if @user.id != session[:id]
       redirect '/sessions/login'
